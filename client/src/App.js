4d67de6796393b5ee063271ee520a02d64f7348b
javiddelossantos5 <javiddelossantos5@gmail.com>
2021-06-12 01:05:54 -0600

    added urls on react so its more user friendly and database connects to front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import Header from './components/Header';
@@ -7,7 +14,7 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 const client = new ApolloClient({
@@ -18,13 +25,23 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
-        <Header />
-        <div className='container'>
-          <Home />
+      <Router>
+        <div className='flex-column justify-flex-start min-100-vh'>
+          <Header />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/login' component={Login}/>
+              <Route exact path='/signup' component={Signup}/>
+              <Route exact path='/profile/:username?' component={Profile}/>
+              <Route exact path='/thought/:id' component={SingleThought}/>
+
+              <Route component={NoMatch}/>
+            </Switch>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>l
     </ApolloProvider>
   );
 }
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,7 +1,45 @@
 import { gql } from '@apollo/client';
 
+import { useQuery } from '@apollo/client';
+
 export const QUERY_THOUGHTS = gql`
     query thoughts($username: String) {
+        thoughts(username: $username) {
+            __id
+            thoughtText
+            createdAt
+            username
+            reactionCount
+            reactions {
+            _id
+            createdAt
+            username
+            reactionBody
+            }
+        }
+    }
+`;
+
+export const QUERY_THOUGHT = gql`
+    query thought($id: ID!) {
+        thought(_id: $id) {
+            _id
+            thoughtText
+            createdAt
+            username
+            reactionCount
+            reactions {
+                _id
+                createdAt
+                username
+                reactionBody
+            }
+        }
+    }
+`;
+
+export const QUERY_USER = gql`
+    query user($username: String!) {
         user(username: $username) {
             _id
             username
