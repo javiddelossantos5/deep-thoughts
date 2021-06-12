f0ee2b0de4390949cb546fcb3a27a7e4a4c2639b
javiddelossantos5 <javiddelossantos5@gmail.com>
2021-06-11 21:59:41 -0600

    added react to add friends and thought to connect to the back end database

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,26 @@
 import React from 'react';
 
+import { useParams } from 'react-router-dom';
+import ThoughtList from '../components/ThoughtList';
+
+import { useQuery } from '@apollo/client';
+import { QUERY_USER } from '../utils/queries';
+
+import FriendList from '../components/FriendList';
+
 const Profile = () => {
+  const { username: userParam } = useParams();
+
+  const { loading, data } = useQuery(QUERY_USER, {
+    variables: { username: userParam }
+  });
+
+  const user = data?.user || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <div className="flex-row mb-3">
@@ -10,9 +30,16 @@ const Profile = () => {
       </div>
 
       <div className="flex-row justify-space-between mb-3">
-        <div className="col-12 mb-3 col-lg-8">{/* PRINT THOUGHT LIST  */}</div>
-
-        <div className="col-12 col-lg-3 mb-3">{/* PRINT FRIEND LIST */}</div>
+        <div className="col-12 mb-3 col-lg-8">
+          <ThoughtList thoughts={user.thoughts} title={`${user.usename}s thoughts...`}/>
+        </div>
+        <div className="col-12 col-lg-3 mb-3">
+          <FriendList
+          username={user.username}
+          friendCount={user.friendCount}
+          friends={user.friends}
+          />
+        </div>
       </div>
     </div>
   );
---
+++ client/src/utils/queries.js
@@ -0,0 +1,22 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_THOUGHTS = gql`
+    query thoughts($username: String) {
+        user(username: $username) {
+            _id
+            username
+            email
+            friendCount
+            friends {
+                _id
+                username
+            }
+            thoughts {
+                _id
+                thoughtText
+                createdAt
+                reactionCount
+            }
+        }
+    }
+`;
